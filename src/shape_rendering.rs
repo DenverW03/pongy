76dd18c3cd9dfbcfa60aa344f209a3a9a644139b
DenverW03 <denverwhiteley@hotmail.com>
2024-08-12 13:59:20 +0100

    two paddles in now

--- src/shape_rendering.rs
+++ src/shape_rendering.rs
@@ -7,52 +7,80 @@ pub struct PaddlePlugin;
 #[derive(Bundle)]
 struct PaddleBundle<M> where M: Material2d {
     sprite: MaterialMesh2dBundle<M>,
-    velocity: Velocity,
+    velocity: PaddleVelocity,
+    direction: Direction,
+    side: Side,
 }
 
 #[derive(Component)]
-pub struct Velocity {
-    xvel: f32,
-    yvel: f32,
-}
+pub struct PaddleVelocity(f32);
+
+#[derive(Component)]
+pub struct Direction(f32); // 1.0 or -1.0 to indicate direction of velocity
+
+#[derive(Component)]
+pub struct Side(String);
 
 impl Plugin for PaddlePlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, add_rectangle);
-        app.add_systems(Update, apply_velocity);
+        app.add_systems(Update, handle_keys);
     }
 }
 
-pub fn add_rectangle(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<ColorMaterial>>) {
+pub fn add_rectangle(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<ColorMaterial>>, window: Query<&Window>) {
     // adding a camera for the meshes
     commands.spawn(Camera2dBundle::default());
 
-    // Adding the paddle bundle
-    commands.spawn(PaddleBundle {
-        sprite: MaterialMesh2dBundle {
-            mesh: Mesh2dHandle(meshes.add(Rectangle::new(1.0, 1.0))),
-            transform: Transform::default().with_scale(Vec3 { x: 100.0, y: 100.0, z: 0.0 }),
-            material: materials.add(Color::from(WHITE)),
-            ..default()
-        },
-        velocity: Velocity { xvel: 0.0, yvel: 0.0 },
-    });
+    if let Ok(window) = window.get_single() {
+        let width = window.resolution.width();
+        let padding = 10.0;
+        let left_off = (-width / 2.0) + padding;
+        let right_off = (width / 2.0) - padding;
+
+        // Adding the left paddle
+        commands.spawn(PaddleBundle {
+            sprite: MaterialMesh2dBundle {
+                mesh: Mesh2dHandle(meshes.add(Rectangle::new(1.0, 1.0))),
+                transform: Transform::from_xyz(left_off, 0.0, 0.0).with_scale(Vec3 { x: 100.0, y: 100.0, z: 0.0 }),
+                material: materials.add(Color::from(WHITE)),
+                ..default()
+            },
+            velocity: PaddleVelocity(0.0),
+            direction: Direction(0.0),
+            side: Side("left".to_string()),
+        });
+
+        // Adding the right paddle
+        commands.spawn(PaddleBundle {
+            sprite: MaterialMesh2dBundle {
+                mesh: Mesh2dHandle(meshes.add(Rectangle::new(1.0, 1.0))),
+                transform: Transform::from_xyz(right_off, 0.0, 0.0).with_scale(Vec3 { x: 100.0, y: 100.0, z: 0.0 }),
+                material: materials.add(Color::from(WHITE)),
+                ..default()
+            },
+            velocity: PaddleVelocity(0.0),
+            direction: Direction(0.0),
+            side: Side("right".to_string()),
+        });
+    }
 }
 
-// Querying entities that have the attributes Transform and Mesh2dHandle (essentially the paddles)
-pub fn apply_velocity(mut query: Query<(&mut Transform, &Mesh2dHandle)>, keys: Res<ButtonInput<KeyCode>>) {
-    for (mut transform, _sprite) in &mut query {
-        if keys.just_pressed(KeyCode::ArrowUp) {
-            transform.translation = Vec3::new(transform.translation.x, transform.translation.y + 10.0, transform.translation.z);
+// Querying entities that have the specific attributes (essentially the paddles)
+pub fn handle_keys(mut query: Query<(&mut Transform, &Mesh2dHandle, &mut PaddleVelocity, &mut Direction, &Side)>, keys: Res<ButtonInput<KeyCode>>) {
+    for (mut transform, _sprite, mut _velocity, mut direction, _side) in &mut query {
+        // Setting the direction of velocity based on the key being down
+        if keys.pressed(KeyCode::ArrowUp) {
+            *direction = Direction(1.0);
         }
-        if keys.just_pressed(KeyCode::ArrowDown) {
-            transform.translation = Vec3::new(transform.translation.x, transform.translation.y - 10.0, transform.translation.z);
+        else if keys.pressed(KeyCode::ArrowDown) {
+            *direction = Direction(-1.0);
         }
-        if keys.just_pressed(KeyCode::ArrowRight) {
-            transform.translation = Vec3::new(transform.translation.x + 10.0, transform.translation.y, transform.translation.z);
+        else {
+            *direction = Direction(0.0);
         }
-        if keys.just_pressed(KeyCode::ArrowLeft) {
-            transform.translation = Vec3::new(transform.translation.x - 10.0, transform.translation.y, transform.translation.z);
-        }
+
+        // Applying the velocity TODO: MAKE THE VELOCITY ACCELERATE AND DECELERATE
+        transform.translation = Vec3::new(transform.translation.x, transform.translation.y + (1.0 * direction.0), transform.translation.z);
     }
 }
