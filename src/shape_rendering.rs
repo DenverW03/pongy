e3fb4d3216755a5aa25a33852bf1eecbd1b535d6
DenverW03 <denverwhiteley@hotmail.com>
2024-08-13 13:17:18 +0100

    separate key control

--- src/shape_rendering.rs
+++ src/shape_rendering.rs
@@ -34,7 +34,7 @@ pub fn add_rectangle(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, m
 
     if let Ok(window) = window.get_single() {
         let width = window.resolution.width();
-        let padding = 10.0;
+        let padding = 100.0;
         let left_off = (-width / 2.0) + padding;
         let right_off = (width / 2.0) - padding;
 
@@ -42,7 +42,7 @@ pub fn add_rectangle(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, m
         commands.spawn(PaddleBundle {
             sprite: MaterialMesh2dBundle {
                 mesh: Mesh2dHandle(meshes.add(Rectangle::new(1.0, 1.0))),
-                transform: Transform::from_xyz(left_off, 0.0, 0.0).with_scale(Vec3 { x: 100.0, y: 100.0, z: 0.0 }),
+                transform: Transform::from_xyz(left_off, 0.0, 0.0).with_scale(Vec3 { x: 20.0, y: 100.0, z: 0.0 }),
                 material: materials.add(Color::from(WHITE)),
                 ..default()
             },
@@ -55,7 +55,7 @@ pub fn add_rectangle(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, m
         commands.spawn(PaddleBundle {
             sprite: MaterialMesh2dBundle {
                 mesh: Mesh2dHandle(meshes.add(Rectangle::new(1.0, 1.0))),
-                transform: Transform::from_xyz(right_off, 0.0, 0.0).with_scale(Vec3 { x: 100.0, y: 100.0, z: 0.0 }),
+                transform: Transform::from_xyz(right_off, 0.0, 0.0).with_scale(Vec3 { x: 20.0, y: 100.0, z: 0.0 }),
                 material: materials.add(Color::from(WHITE)),
                 ..default()
             },
@@ -68,18 +68,34 @@ pub fn add_rectangle(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, m
 
 // Querying entities that have the specific attributes (essentially the paddles)
 pub fn handle_keys(mut query: Query<(&mut Transform, &Mesh2dHandle, &mut PaddleVelocity, &mut Direction, &Side)>, keys: Res<ButtonInput<KeyCode>>) {
-    for (mut transform, _sprite, mut _velocity, mut direction, _side) in &mut query {
+    for (mut transform, _sprite, mut _velocity, mut direction, side) in &mut query {
         // Setting the direction of velocity based on the key being down
-        if keys.pressed(KeyCode::ArrowUp) {
-            *direction = Direction(1.0);
+        match side.0.as_str() {
+            "left" => { // the left paddle
+                    if keys.pressed(KeyCode::KeyW) {
+                            *direction = Direction(1.0);
+                    }
+                    else if keys.pressed(KeyCode::KeyS) {
+                        *direction = Direction(-1.0);
+                    }
+                    else {
+                        *direction = Direction(0.0);
+                    }
+                },
+            "right" => { // the right paddle
+                    if keys.pressed(KeyCode::ArrowUp) {
+                        *direction = Direction(1.0);
+                    }
+                    else if keys.pressed(KeyCode::ArrowDown) {
+                        *direction = Direction(-1.0);
+                    }
+                    else {
+                        *direction = Direction(0.0);
+                    }
+                },
+            _ => {}, // do nothing, wildcard can never occur anyway
         }
-        else if keys.pressed(KeyCode::ArrowDown) {
-            *direction = Direction(-1.0);
-        }
-        else {
-            *direction = Direction(0.0);
-        }
-
+        
         // Applying the velocity TODO: MAKE THE VELOCITY ACCELERATE AND DECELERATE
         transform.translation = Vec3::new(transform.translation.x, transform.translation.y + (1.0 * direction.0), transform.translation.z);
     }
